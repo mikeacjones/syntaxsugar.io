246cc14d07a505b4b2a0a00adab3c04410f1535d
Michael Jones <mjones@kotrs.com>
2020-12-22 18:32:04 -0500

    Fixing package.json file

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import './SideBar.css'
-import profilePicture from '../../public/profilePicture.png'
+import profilePicture from '../../static/profilePicture.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faGithub,
