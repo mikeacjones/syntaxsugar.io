d2982ee3656eff8a507f955f963cac9a5ea294ac
Michael Jones <mjones@kotrs.com>
2020-12-24 15:39:24 -0500

    Videos should not push out of frame

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,9 +48,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-embed-video',
             options: {
-              width: 800,
-              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
+              width: '100%',
               related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
               noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
               containerClass: 'embedVideo-container', //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
