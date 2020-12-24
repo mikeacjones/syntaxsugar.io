34b4866a3a5599edc1b25f1f93b76b46cde474cf
Michael Jones <mjones@kotrs.com>
2020-12-24 14:03:44 -0500

    Moved ZoomableImage css out of js

--- src/components/ZoomableImage.js
+++ src/components/ZoomableImage.js
@@ -1,25 +1,10 @@
 import React from 'react'
+import './ZoomableImage.css'
 
 export default ( props ) => {
   const [enlarged, setEnlarged] = React.useState(false)
   const toggleEnlarge = () => {
     setEnlarged(!enlarged)
   }
-  return <div style={!enlarged ? {
-    maxWidth: '100%',
-    width: '700px',
-    overflow: 'hidden',
-    cursor: 'pointer',
-  } : {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    cursor: 'pointer',
-    display: 'flex',
-    padding: '50px',
-    zIndex: 100,
-    backgroundColor: 'rgba(0, 0, 0, .8)',
-  }} onClick={toggleEnlarge}>{props.children}</div>
+  return <div className={enlarged ? 'ZoomableImage-enlarged' : 'ZoomableImage'} onClick={toggleEnlarge}>{props.children}</div>
 }
