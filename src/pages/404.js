f5571c44849c315232de87eeeb8959c4a684d2bc
Michael Jones <mjones@kotrs.com>
2020-12-26 02:27:33 -0500

    External links open new tab + added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Layout } from '../components/Layout'
+import confusedMan from '../../static/images/dizzy.svg'
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div className='post-view'>
+        <div className='post-view-content'>
+          <img src={confusedMan} alt='confused-man' />
+          <div className='post-view-header'>
+            <div className='post-view-title'>
+              <h1>Uh oh.. I can't find that page!</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
