7cb788bb5e8849f7b0671b8d5be953f1b35e35e7
Michael Jones <mjones@kotrs.com>
2021-01-01 22:47:12 -0500

    Fix font awesome css bug

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { SideBar } from './sidebar/SideBar'
 import { ContentWrapper } from './ContentWrapper'
 import { SideBarToggle } from './sidebar/SideBarToggle'
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 import './Layout.css'
 
